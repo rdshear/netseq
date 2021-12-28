c3f0b1a110d467a8d24499fb7e3283f9a6d8aa84
Robert D Shear <rshear@gmail.com>
2021-12-28 16:39:22 -0500

    Pull from SRA and maxSpotCount implmented

--- test/local-docstore-netseq.sh
+++ test/local-docstore-netseq.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 # local-docstore-netseq.sh
-dockstore workflow launch --local-entry ~/Projects/netseq/netseq.wdl --json ~/Projects/netseq/test/inputs.json
+dockstore workflow launch --local-entry ~/Projects/netseq/netseq.wdl --json ~/Projects/netseq/test/test-2/inputs.json
