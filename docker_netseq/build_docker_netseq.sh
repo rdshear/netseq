027b811ee2f1781e35ac5628b09a2b591bfd75d0
Robert D Shear <rshear@gmail.com>
2021-12-28 09:31:57 -0500

    bump version to 0.0.3, better build_docker script

--- docker_netseq/build_docker_netseq.sh
+++ docker_netseq/build_docker_netseq.sh
@@ -2,4 +2,7 @@ cd /Users/robertshear/Projects/netseq/docker_netseq
 imagename=rdshear/netseq
 tagname=${imagename}:$(cat ../VERSION)
 docker build . -t ${tagname}
+docker tag ${tagname} ${imagename}:latest
 docker push ${tagname}
+docker push ${imagename}:latest
+docker system prune -f
