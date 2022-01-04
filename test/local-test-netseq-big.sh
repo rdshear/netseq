3dcaf03e11c5d7c6ddc23984ee0df20aca0b710c
Robert D Shear <rshear@gmail.com>
2022-01-04 06:50:14 -0500

    cleanup test file structure

--- test/local-test-netseq-big.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cd ~/temp || exit
-rm -R ~/temp/*
-
-conda activate cpa
-
-cromwell run -i ~/Projects/netseq/test/test-2/local_inputs_big.json -t wdl \
-   -o /Users/robertshear/Projects/netseq/test/options.json \
-   ~/Projects/netseq/netseq.wdl 
